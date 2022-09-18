282c65c2380753939ed63d59d0c2998a8ebe6ad7
joshbalsicas <joshbalsicas@hotmail.com>
2022-09-18 06:40:20 -0400

    added button to layout

--- app/src/main/java/ca/georgiancollege/comp3025_lab1/MainActivity.kt
+++ app/src/main/java/ca/georgiancollege/comp3025_lab1/MainActivity.kt
@@ -2,10 +2,21 @@ package ca.georgiancollege.comp3025_lab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    //LifeCycle Function
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        val HelloWorldLabel = findViewById(R.id.Hello_World_Label) as TextView
+        val ClickMeButton = findViewById(R.id.Click_Me_Button) as Button
+
+        ClickMeButton.setOnClickListener {
+            HelloWorldLabel.text = "Clicked!"
+        }
     }
 }
